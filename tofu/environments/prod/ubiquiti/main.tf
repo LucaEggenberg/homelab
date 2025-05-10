f0d26168dda054c716a6eb071c760818ec1a51db
Luca Eggenberg <luca.e@outlook.com>
2025-05-10 10:58:46 +0200

    configure network

--- tofu/environments/prod/ubiquiti/main.tf
+++ tofu/environments/prod/ubiquiti/main.tf
@@ -1,3 +1,75 @@
+### Network ###
+resource "unifi_network" "private_vlan" {
+    name        = "Private"
+    purpose     = "vlan-only"
+    subnet      = "10.10.10.0/24"
+    dhcp_dns    = ["10.10.10.1", "1.1.1.1", "1.0.0.1"]
+    dhcp_enabled = true
+    dhcp_start  = "10.10.10.50"
+    dhcp_stop   = "10.10.10.254"
+    vlan_id     = 1
+}
+
+resource "unifi_network" "public_vlan" {
+    name        = "Public"
+    purpose     = "vlan-only"
+    subnet      = "10.10.20.0/24"
+    dhcp_enabled = true
+    dhcp_start  = "10.10.20.100"
+    dhcp_stop   = "10.10.20.254"
+    vlan_id     = 2
+}
+
+resource "unifi_network" "iot_vlan" {
+    name        = "Public"
+    purpose     = "vlan-only"
+    subnet      = "10.10.20.0/24"
+    dhcp_enabled = true
+    dhcp_start  = "10.10.30.50"
+    dhcp_stop   = "10.10.30.254"
+    vlan_id     = 3
+}
+
+### Dns-Entries ###
+resource "unifi_dns_record" "srv-plg-1" {
+    name   = "srv-plg-1.eggenberg.io"
+    type   = "A"
+    record = "10.10.20.11"
+}
+
+resource "unifi_dns_record" "srv-plg-1_cname" {
+
+    for_each = toset([
+       "gameserver"
+    ])
+
+    name   = join(".", [each.key, "srv-plg-1.eggenberg.io"])
+    type   = "CNAME"
+    record = "srv-plg-1.eggenberg.io"
+}
+
+resource "unifi_dns_record" "kube" {
+    name   = "kube.eggenberg.io"
+    type   = "A"
+    record = "10.10.20.40"
+}
+
+resource "unifi_dns_record" "kube_cname" {
+
+    for_each = toset([
+        "argocd",
+        "longhorn",
+        "prowlarr",
+        "sonarr",
+        "radarr"
+    ])
+
+    name   = join(".", [each.key, "kube.eggenberg.io"])
+    type   = "CNAME"
+    record = "kube.eggenberg.io"
+}
+
+### Port-Forwards ###
 resource "unifi_port_forward" "cloudflare_http" {
     for_each = toset([
         "173.245.48.0/20",
