6dd7bc105b75d37530a2d15fa4ad781640ab6b1b
= <luca.e@outlook.com>
2025-09-20 18:08:41 +0200

    correctly configure ssh-key

--- infrastructure/nixos/users/luca/default.nix
+++ infrastructure/nixos/users/luca/default.nix
@@ -3,9 +3,8 @@
         isNormalUser = true;
         extraGroups = [ "wheel" "networkmanager" ];
         shell = pkgs.bash;
-    };
-
-    services.openssh.knownHosts = {
-        luca.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPLTvjhfe4YWatJ3Y19rYj8YHGmiki5AMqDykfkFH5/f";
+        openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPLTvjhfe4YWatJ3Y19rYj8YHGmiki5AMqDykfkFH5/f"
+        ];
     };
 }
