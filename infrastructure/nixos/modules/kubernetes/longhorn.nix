af1151929d7b4ec263fba8d369514c8f1b2e78f2
= <luca.e@outlook.com>
2025-09-20 21:51:01 +0200

    add better name & iscsi package

--- infrastructure/nixos/modules/kubernetes/longhorn.nix
+++ infrastructure/nixos/modules/kubernetes/longhorn.nix
@@ -5,11 +5,12 @@
         util-linux # for findmnt, blkid, lsblk
         gnugrep
         gawk
-
+        openiscsi
     ];
 
     services.openiscsi = {
         enable = true;
+        name = "iqn.2025-09.com.homelab:p-kube-1";
     };
 
     services.rpcbind.enable = true; # needed for NFS
