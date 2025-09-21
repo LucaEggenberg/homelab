1d7bd42b4afc11d6bb00e27bd9b3f1b1e609249a
= <luca.e@outlook.com>
2025-09-21 12:19:30 +0200

    add node 3

---
+++ infrastructure/nixos/hosts/p-kube-3/default.nix
@@ -0,0 +1,15 @@
+{ ... }: {
+    networking.hostName = "p-kube-3";
+    
+    imports = [
+        ./hardware-configuration.nix
+    ];
+
+    sops = {
+        age.sshKeyPaths = [ "/etc/ssh/id_ed25519" ];
+
+        secrets."k3s/token" = {
+            sopsFile = ../../secrets/kubernetes/k3s.yaml;
+        };
+    };
+}
