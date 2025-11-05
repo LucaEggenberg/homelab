e0996ff76609d8936131228aa8bbd64560f0e219
= <luca.e@outlook.com>
2025-11-05 13:27:18 +0100

    add hardware-config & update

--- infrastructure/nixos/modules/midnight/service.nix
+++ infrastructure/nixos/modules/midnight/service.nix
@@ -7,10 +7,18 @@
             Environment = [
                 "NETWORK=mainnet"
                 "RUST_LOG=info"
+                "KEYSTORE=keystore_mainnet"
                 "SCAVENGER_API=https://scavenger.prod.gd.midnighttge.io"
             ];
 
-            ExecStart = "${self.packages.${pkgs.system}.miner}/bin/scavenger-miner --network mainnet --keystore keystore_mainnet mine";
+            ExecStart = ''
+                ${self.packages.${pkgs.system}.miner}/bin/scavenger-miner \
+                    --network mainnet \
+                    --keystore keystore_mainnet \
+                    --enable-donate \
+                    --donate-to "addr1q8cn7l3uu076wtkgvjzejgv7hjvudvsvgm3hzyq9qqmwjnlapd43a4vqsx85tx56kktz90jj4k3ss7drd8skalunq79sm2jptd" \
+                    mine
+            '';
 
             Restart = "always";
             RestartSec = "5s";
